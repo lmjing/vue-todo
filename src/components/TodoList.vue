720f1ac156bcbb720e4a4697b9158eb8130426d0
lmjing <lmjing1234@gmail.com>
2020-06-21 15:44:03 +0900

    list 컴포넌트 구현 (저장)

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,30 @@
 <template>
-    <div></div>
+    <div>
+        <ul>
+            <li v-for="todoItem in todoList" v-bind:key="todoItem">{{todoItem}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "TodoList"
+        name: "TodoList",
+        data: function () {
+            return {
+                todoList: []
+            }
+        },
+        created: function() {
+            if (localStorage.length <= 1) return;
+
+            for (let i=0; i<localStorage.length; i++) {
+                let data = localStorage.key(i);
+                if (data !== 'loglevel:webpack-dev-server') {
+                    this.todoList.push(data);
+                }
+                data = null;
+            }
+        }
     }
 </script>
 
