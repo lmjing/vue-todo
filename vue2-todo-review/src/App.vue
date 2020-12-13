574ca39877afd9006b937ba13b6761d3262f37f6
lmjing <lmjing1234@gmail.com>
2020-12-13 11:26:34 +0900

    [복습] 프로젝트 구성, 컴포넌트 생성 및 등록

---
+++ vue2-todo-review/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+  </v-app>
+</template>
+
+<script>
+  import TodoHeader from "./components/TodoHeader";
+  import TodoInput from "./components/TodoInput";
+  import TodoList from "./components/TodoList";
+  import TodoFooter from "./components/TodoFooter";
+
+export default {
+  name: 'App',
+
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
