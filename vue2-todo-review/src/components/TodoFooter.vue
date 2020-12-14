5def17bed31e538c07191969983b06cd5b0dd32f
lmjing <lmjing1234@gmail.com>
2020-12-14 23:30:18 +0900

    [복습] Footer 구현 - Clear All

--- vue2-todo-review/src/components/TodoFooter.vue
+++ vue2-todo-review/src/components/TodoFooter.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        footer
+    <div class="pa-5">
+        <v-btn block outlined
+               color="teal"
+               @click="clearAll"
+        >
+            Clear All
+        </v-btn>
     </div>
 </template>
 
 <script>
     export default {
-        name: "TodoFooter"
+        name: "TodoFooter",
+        methods: {
+            clearAll () {
+                localStorage.clear();
+            }
+        }
     }
 </script>
 
