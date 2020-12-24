2bec52935f74480f0b8c40012e3d5b3de6dd3b78
lmjing <lmjing1234@gmail.com>
2020-12-24 15:50:12 +0900

    [복습] 3. vuex 적용 - 설치

--- vue2-todo-review/src/main.js
+++ vue2-todo-review/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import vueMoment from 'vue-moment';
+import { store } from './store/store';
 
 Vue.config.productionTip = false;
 Vue.use(vueMoment);
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app');
